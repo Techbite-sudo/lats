65f0d33b8aa8d49284f24bf5c71ce99e6e4c2952
Techbite <bonfacemwema7@gmail.com>
2023-04-02 02:23:02 +0300

    Tried to integrate flowbite tailwind css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+@vite(['resources/css/app.css','resources/js/app.js'])
 @extends('layouts.app')
 
 @section('content')
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
